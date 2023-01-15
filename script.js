ccdfa8caf1191077d119a53ea950aa3ecad44051
Tatiana Johnson <Tatianarjo@gmail.com>
2023-01-14 18:01:12 -0600

    added functions for different lines of chat

--- script.js
+++ script.js
@@ -1,23 +1,64 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
+import bot from "./assets/bot.svg";
+import user from "./assets/user.svg";
 
+const form = document.querySelector("form");
+const chatContainer = document.querySelector("#chat_container");
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>askIcyWhites</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
+let loadInterval;
+
+function loader(element) {
+  element.textContent = "";
+  //loads answers and loads the three dots that will show; this will repeat every 300 milliseconds
+  loadInterval = setInterval(() => {
+    element.textContent += ".";
+
+    if (element.textContent === "....") {
+      element.textContent = "";
+    }
+  }, 300);
+}
+//this function makes the letters of the answers appear
+function typeText(element, text) {
+  let index = 0;
+
+  let interval = setInterval(() => {
+    if (index < text.length) {
+      element.innerHTML += text.chartAt(index);
+      index++;
+    } else {
+      clearInterval(interval);
+    }
+  }, 20);
+}
+
+//create a unique id to map over them; using a timestamp and function to create a random number I get a unique id
+function generateUniqueId() {
+  const timestamp = Date.now();
+  const randomNumber = Math.random();
+  const hexadecimalString = randomNumber.toString(16);
+
+  return `id-${timestamp}-${hexadecimalString}`;
+}
+
+//creating a function for the different lines of chat
+
+function chatStripe(isAi, value, uniqueId) {
+  return ( `
+    <div class="wrapper ${isAi && 'ai'}">
+    <div class="chat">
+    <div className="profile">
+    <img src="${isAi ? bot : user}"
+    alt="${isAi ? 'bot' : 'user'}"
+    />
     </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+    <div class="message" id=${uniqueId}>${value}></div>
+    </div>
+    </div>
+    `);
+}
 
-setupCounter(document.querySelector('#counter'))
+//creates the submission to get the AI response
+//prevents the reload of the browser
+const handleSubmit = async (e) => {
+  e.preventDefault();
+}
